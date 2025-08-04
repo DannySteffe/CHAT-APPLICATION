2118ef144f2e19dfa5c40b5ce5f96cddb96cb6a4
Danny Steffe S <dannysteffe1465@gmail.com>
2025-08-04 06:26:01 -0800

    Add login feature

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProfileUpdate.css';
 import assets from '../../assets/assets';
 
 const ProfileUpdate = () => {
+
+const [image,setImage] = useState(false)
+
+
   return (
     <div>
-<div className="profile">
+<div className="profile"> 
   <div className="profile-container">
     <form>
-      <h3>Profile Details here</h3>
+      <h3>Profile Details</h3>
       <label htmlFor='avatar'>
-        <input type='file' id='avatar' accept='.png, .jpg, .jpeg' hidden />
-        <img src={assets.avatar_icon} alt=''/>
+        <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='avatar' accept='.png, .jpg, .jpeg' hidden />
+        <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt=''/>
         Upload profile image
       </label>
       <input type='text' placeholder='your name' required  />
       <textarea placeholder='Write profile bio' required></textarea>
       <button type='submit'>Save</button>
       </form>
-      <img className='profile-pic' src={assets.logo_icon} required-hidden/>
+      <img className='profile-pic' src={image?URL.createObjectURL(image) : assets.logo_icon} required-hidden/>
   </div>
   </div>
      </div>
     
   );
 };
-
 export default ProfileUpdate;
