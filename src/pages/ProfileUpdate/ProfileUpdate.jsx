0361e2bb98da7267e81f5257cf9f22379a1c9a76
Danny Steffe S <dannysteffe1465@gmail.com>
2025-08-03 22:12:19 -0800

    changes in profileupdate

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -8,7 +8,7 @@ const ProfileUpdate = () => {
 <div className="profile">
   <div className="profile-container">
     <form>
-      <h3>Profile Details</h3>
+      <h3>Profile Details here</h3>
       <label htmlFor='avatar'>
         <input type='file' id='avatar' accept='.png, .jpg, .jpeg' hidden />
         <img src={assets.avatar_icon} alt=''/>
