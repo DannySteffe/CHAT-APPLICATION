c112be85d6280b83fd0eab4bd93cd1f5270e9eac
Danny Steffe S <dannysteffe1465@gmail.com>
2025-08-05 08:54:55 -0800

    importing react toastify

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyC0XUhb9Wh0VWILUocLF-lg2ivsLfwEfb8",
@@ -16,11 +19,11 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app)
  
-const signup - async (username,email,password) => {
+const signup = async (username,email,password) => {
     try {
         const res = await createUserWithEmailAndPassword (auth, email, password);
         const user = res.user;
-        await SendToBack(doc(db, "user.id",{
+        await SendToBack(doc(db, "users",user.id),{
             id:user.uid,
             username:username.toLowerCase(),
             email,
@@ -29,6 +32,13 @@ const signup - async (username,email,password) => {
             bio:"hey, there i an using chat app",
             lastseen:Date.now()
             
-        }))
+        }
+        )
+        await setDoc(doc(db, "Chats", user.uid), {
+            chatData:[]
+        })
+    } catch (error) {
+        console.error(Error );
+
     }
 }
